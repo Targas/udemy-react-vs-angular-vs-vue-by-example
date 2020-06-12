0b249e481016611a78d842c5904d7b8c88486a13
danilo targas <danilo@adalov.com>
2020-06-11 22:45:33 -0300

    adicionado menu no vue

--- compare-vue/src/App.vue
+++ compare-vue/src/App.vue
@@ -1,9 +1,42 @@
 <template>
   <div id="app">
-    <div id="nav">
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </div>
+    <nav class="navbar" role="navigation">
+      <div class="container">
+        <div class="navbar-brand">
+          <router-link to="/">
+            <img src="http://pr8.com.br/targas/images/profile.jpg" class="logo">
+          </router-link>
+
+          <a role="button" class="navbar-burger burger" aria-label="menu" aria-expanded="false">
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+            <span aria-hidden="true"></span>
+          </a>
+        </div>
+
+        <div id="navbarBasicExample" class="navbar-menu">
+          <div class="navbar-end">
+            <div class="navbar-start">
+              <router-link to="/about" class="navbar-item r-item">About</router-link>
+              <router-link to="/faq" class="navbar-item r-item">Faq</router-link>
+              <router-link to="/features" class="navbar-item r-item">Features</router-link>
+            </div>
+            
+              <div class="navbar-item">
+              <div class="buttons">
+                <a class="button is-primary">
+                  <strong>
+                    <i class="fa fa-sign-in"></i>
+                    Join now
+                  </strong>
+                </a>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+
     <router-view/>
   </div>
 </template>
@@ -11,4 +44,16 @@
 <style lang="scss">
 @import '../node_modules/bulma/bulma.sass';
 @import 'mq';
+
+.logo {
+  width: 50px;
+  height: 50px;
+}
+.navbar {
+  background-color: #383838;
+}
+.navbar-item {
+  color: #FFFFFF;
+  padding: 0 10px;
+}
 </style>
--- compare-vue/src/views/Faq.vue
+++ compare-vue/src/views/Faq.vue
@@ -14,3 +14,7 @@ export default {
   }
 }
 </script>
+
+<style>
+  @import '../mq.scss';
+</style>
